bccdea966cb7426cf4975af7a072a7bea66a820b
khushboojain93 <khushboojainmca@gmail.com>
2020-07-28 11:58:15 +0530

    Initial commit

---
+++ src/main/java/com/infogain/springframework5/Springframework5Application.java
@@ -0,0 +1,13 @@
+package com.infogain.springframework5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springframework5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springframework5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/infogain/springframework5/Springframework5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infogain.springframework5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springframework5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
