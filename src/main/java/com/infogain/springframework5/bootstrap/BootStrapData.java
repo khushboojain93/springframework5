d4666c6c75dcfa4a334b52b62b7197e90abd26b4
khushboojain93 <khushboojainmca@gmail.com>
2020-08-01 19:53:31 +0530

    added publisher relationship

--- src/main/java/com/infogain/springframework5/bootstrap/BootStrapData.java
+++ src/main/java/com/infogain/springframework5/bootstrap/BootStrapData.java
@@ -9,6 +9,9 @@ import com.infogain.springframework5.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Component
 public class BootStrapData implements CommandLineRunner {
 
@@ -25,16 +28,28 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Author eric=new Author("Eric", "Evans");
-        Book ericBook=new Book("Domain Driven Design", "123123");
-        eric.getBooks().add(ericBook);
-        ericBook.getAuthors().add(eric);
+        Author author1=new Author("Eric", "Evans");
+        Book book1=new Book("Domain Driven Design", "123123");
+        author1.getBooks().add(book1);
+        book1.getAuthors().add(author1);
 
-        authorRepository.save(eric);
-        bookRepository.save(ericBook);
+        authorRepository.save(author1);
+        bookRepository.save(book1);
 
+        Author author2=new Author("Nicholas","Sparks");
+        Book book2=new Book("TFIOS","4515164");
+
+        author2.getBooks().add(book2);
+        book2.getAuthors().add(author2);
+
+        authorRepository.save(author2);
+        bookRepository.save(book2);
         System.out.println("Started in BootStrap");
         System.out.println("No of books: "+bookRepository.count());
+
+        Set<Book> bookSet=new HashSet<>();
+        bookSet.add(book1);
+        bookSet.add(book2);
 
         Publisher publisher=new Publisher();
         publisher.setName("Assignment-publisher");
@@ -42,10 +57,23 @@ public class BootStrapData implements CommandLineRunner {
         publisher.setCity("seattle");
         publisher.setState("United States");
         publisher.setZip("Publisher-zip");
+        //publisher.getBooks().add(book1);
+        //publisher.getBooks().add(book2);
+        publisher.setBooks(bookSet);
 
         publisherRepository.save(publisher);
 
         System.out.println("No of publishers: "+publisherRepository.count());
+        System.out.println("No of books published by publisher: "+publisher.getBooks().size());
+        
+
+        book1.setPublisher(publisher);
+        book2.setPublisher(publisher);
+
+        System.out.println("Publisher for "+book1.getTitle()+" is : "+book1.getPublisher().getName());
+        System.out.println("Publisher for "+book2.getTitle()+" is : "+book2.getPublisher().getName());
+
+
 
 
     }
--- src/main/java/com/infogain/springframework5/domain/Book.java
+++ src/main/java/com/infogain/springframework5/domain/Book.java
@@ -15,9 +15,10 @@ public class Book {
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
+
     @ManyToMany
-
-
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors=new HashSet<>();
@@ -60,6 +61,14 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     @Override
